abdb729193c40f7477fda400dcc89647f0c86073
delsin <1749970370@qq.com>
2026-05-06 05:11:47 -0400

    update benchmarks (longbench infinitebench ruler niah)

--- data/ruler/download_qa_dataset.sh
+++ data/ruler/download_qa_dataset.sh
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json -O squad.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json -O squad.json
 wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json -O hotpotqa.json
